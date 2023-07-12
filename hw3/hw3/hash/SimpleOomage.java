f89014be4abd2d9c43fca92201a27417beb090a6
disjfjdizmfnkf <3037650079@qq.com>
2023-07-12 16:07:36 +0800

    完成SimpleOomage

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -1,8 +1,10 @@
 package hw3.hash;
+
 import java.awt.Color;
+import java.net.SocketImpl;
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdDraw;
-
 
 public class SimpleOomage implements Oomage {
     protected int red;
@@ -10,26 +12,28 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
         // TODO: Write this method.
-        return false;
+        if (o == this){return true;}
+        if (o == null){return false;}
+        if (o.getClass() != this.getClass()){return false;} //验证o的类型是否是SimpleOomage
+        SimpleOomage that = (SimpleOomage)o; //保证强制类型转换有作用之后进行转换避免出错
+        return (this.blue == that.blue)&&(this.red == that.red)&&(this.green == that.green);
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
+
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
             // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            return (red / 5 * 54 + green / 5) * 54 + blue / 5;      //注意r g b都是5的倍数
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
@@ -67,4 +71,4 @@ public class SimpleOomage implements Oomage {
     public String toString() {
         return "R: " + red + ", G: " + green + ", B: " + blue;
     }
-} 
+}
