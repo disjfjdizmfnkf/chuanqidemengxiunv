4791f6267e2082eb9ad3689d9a3c0b2695291a40
disjfjdizmfnkf <3037650079@qq.com>
2023-09-12 23:05:28 +0800

    test

--- lab8/TestMyHashMapExtra.java
+++ lab8/TestMyHashMapExtra.java
@@ -48,7 +48,6 @@ public class TestMyHashMapExtra {
         assertTrue(q.containsKey("b"));
         assertTrue(q.containsKey("f"));
     }
-
     public static void main(String[] args) {
         jh61b.junit.TestRunner.runTests(TestMyHashMapExtra.class);
     }
